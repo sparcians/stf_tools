db806c7caed3b03899a1eaf36da33a267ba1fcfa
Brett Dutro <brett.dutro@sifive.com>
2022-03-04 09:49:24 -0800

    stf_check: Make masked vector load/store checks more robust

--- tools/stf_check/stf_check.cpp
+++ tools/stf_check/stf_check.cpp
@@ -71,17 +71,67 @@ static STFCheckConfig parse_command_line (int argc, char **argv) {
     return config;
 }
 
-inline bool isVLZero(const stf::STFInst& inst) {
+inline bool isVectorMemAccessMasked(const stf::STFInst& inst) {
     if(inst.isVector()) {
-        for(const auto& op: inst.getSourceOperands()) {
-            if(op.getReg() == stf::Registers::STF_REG::STF_REG_CSR_VL) {
-                if(op.getScalarValue() == 0) {
-                    return true;
+        const auto& operands = inst.getSourceOperands();
+        const auto end_it = operands.end();
+        auto vl = end_it;
+        auto v0 = end_it;
+
+        for(auto it = operands.begin(); it != operands.end(); ++it) {
+            const auto reg_num = it->getReg();
+
+            if(STF_EXPECT_FALSE(reg_num == stf::Registers::STF_REG::STF_REG_CSR_VL)) {
+                vl = it;
+            }
+            else if(STF_EXPECT_FALSE(reg_num == stf::Registers::STF_REG::STF_REG_V0)) {
+                v0 = it;
+            }
+            if(STF_EXPECT_FALSE((vl != end_it) && (v0 != end_it))) {
+                break;
+            }
+        }
+
+        // If there is no VL specified, there can't be any mask
+        if(vl == end_it) {
+            return false;
+        }
+        else {
+            auto vlen = vl->getScalarValue();
+
+            // If VL == 0, all accesses are masked
+            if(vlen == 0) {
+                return true;
+            }
+            // If V0 is specified, bits [VL-1:0] define the mask
+            // If all of the mask bits are 0, then all load/store accesses are masked
+            else if(v0 != end_it) {
+                // Calculate how many vector elements are needed to hold the mask
+                const auto num_mask_elements = stf::InstRegRecord::calcVectorLen(vlen);
+                stf_assert(num_mask_elements != 0, "There should be at least 1 mask element if vlen != 0");
+
+                const auto& v0_data = v0->getVectorValue();
+                using ElementType = std::remove_reference_t<decltype(v0_data)>::value_type;
+
+                // This loop handles the first n-1 mask elements - each one is a full vector element,
+                // so we can do a simple equality check with 0
+                for(size_t i = 0; i < num_mask_elements - 1; ++i) {
+                    if(STF_EXPECT_FALSE(v0_data[i] != 0)) {
+                        return false;
+                    }
+                    // Subtract the number of bits in this element from vlen so we can calculate
+                    // the mask in the final step
+                    vlen -= stf::byte_utils::bitSize<ElementType>();
                 }
-                return false;
+
+                // Check the final mask element - this one may not take up a full vector element,
+                // so we check with a mask instead
+                const auto final_element_mask = stf::byte_utils::bitMask<ElementType>(vlen);
+                return (v0_data[num_mask_elements - 1] & final_element_mask) == 0;
             }
         }
     }
+
     return false;
 }
 
@@ -221,9 +271,8 @@ msg     << "STF_CONTAIN_PHYSICAL_ADDRESS not set, but is required as part of the
                     }
                 }
                 */
-                // Special handling for vector loads
-                // If the VL register is 0, the load won't do any accesses (this is allowed by the spec)
-                found = isVLZero(inst_prev);
+                // Check for special cases where all vector loads are masked
+                found = isVectorMemAccessMasked(inst_prev);
 
                 if(STF_EXPECT_FALSE(!found)) {
                     ecount.countError(ErrorCode::MISS_MEM);
@@ -249,9 +298,8 @@ msg     << "STF_CONTAIN_PHYSICAL_ADDRESS not set, but is required as part of the
                     }
                 }
                 */
-                // Special handling for vector stores
-                // If the VL register is 0, the store won't do any accesses (this is allowed by the spec)
-                found = isVLZero(inst_prev);
+                // Check for special cases where all vector stores are masked
+                found = isVectorMemAccessMasked(inst_prev);
 
                 if (!found) {
                     ecount.countError(ErrorCode::MISS_MEM);
