04dd7d7b3f649f2c12556f0fff84f44a90e610e5
Brett Dutro <brett.dutro@sifive.com>
2023-07-21 15:44:43 -0700

    stf_function_histogram: Update to work with new STFSymbolTable interface

--- tools/stf_function_histogram/stf_function_histogram.hpp
+++ tools/stf_function_histogram/stf_function_histogram.hpp
@@ -1,25 +1,25 @@
-#include "stf_elf.hpp"
+#include "stf_symbol_table.hpp"
 #include "print_utils.hpp"
 
 class SymbolHistogram {
     private:
         static inline constexpr std::string_view INLINE_SUFFIX = " (inlined)";
 
-        STFElf stf_elf_;
-        std::unordered_map<const STFElf::Symbol*, uint64_t> symbol_counts_;
+        STFSymbolTable symbol_table_;
+        std::unordered_map<const STFSymbol*, uint64_t> symbol_counts_;
 
     public:
-        explicit SymbolHistogram(const std::string& elf) {
-            stf_elf_.open(elf);
-            stf_elf_.readSymbols();
+        explicit SymbolHistogram(const std::string& elf) :
+            symbol_table_(elf)
+        {
         }
 
         inline bool validPC(const uint64_t pc) const {
-            return !!stf_elf_.findFunction(pc);
+            return !!symbol_table_.findFunction(pc);
         }
 
         inline bool count(const uint64_t pc) {
-            auto result = stf_elf_.findFunction(pc);
+            auto result = symbol_table_.findFunction(pc);
             if(result) {
                 ++symbol_counts_[result.get()];
             }
@@ -29,7 +29,7 @@ class SymbolHistogram {
         inline void dump() const {
             int max_symbol_length = 0;
 
-            std::multimap<uint64_t, const STFElf::Symbol*> sorted_counts;
+            std::multimap<uint64_t, const STFSymbol*> sorted_counts;
 
             for(const auto& symbol_pair: symbol_counts_) {
                 const auto& symbol = *symbol_pair.first;
@@ -58,14 +58,14 @@ class SymbolHistogram {
 
                 const auto& address_ranges = symbol.getRanges();
                 auto range_it = address_ranges.begin();
-                stf::print_utils::printHex(range_it->first);
+                stf::print_utils::printHex(range_it->startAddress());
                 stf::print_utils::printSpaces(4);
                 stf::print_utils::printDec(it->first, 16, ' ');
                 std::cout << std::endl;
 
                 for(++range_it; range_it != address_ranges.end(); ++range_it) {
                     stf::print_utils::printSpaces(static_cast<size_t>(max_symbol_length) + 4);
-                    stf::print_utils::printHex(range_it->first);
+                    stf::print_utils::printHex(range_it->startAddress());
                     std::cout << std::endl;
                 }
             }
