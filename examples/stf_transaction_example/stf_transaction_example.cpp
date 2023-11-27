edf06146067fbdb7e38888afba8ea457d24c983e
Brett Dutro <brett.dutro@sifive.com>
2023-11-27 12:50:16 -0800

    stf_transaction_example: Use stf::type_utils::none in TileLink constructors

--- examples/stf_transaction_example/stf_transaction_example.cpp
+++ examples/stf_transaction_example/stf_transaction_example.cpp
@@ -4,7 +4,7 @@
 #include "stf_transaction_writer.hpp"
 #include "protocols/tilelink.hpp"
 
-int main(int argc, char** argv) {
+void tilelink_example() {
     stf::STFTransactionWriter writer("test.zstf");
     writer.addTraceInfo(stf::TraceInfoRecord(stf::STF_GEN::STF_TRANSACTION_EXAMPLE, 1, 0, 0, "STF transaction example"));
     writer.setTraceFeature(stf::TRACE_FEATURES::STF_CONTAIN_TRANSACTIONS);
@@ -13,14 +13,14 @@ int main(int argc, char** argv) {
     writer.finalizeHeader();
 
     stf::RecordIdManager id_manager;
-    writer << stf::protocols::TileLink::makeTransaction<stf::protocols::tilelink::ChannelA>(id_manager, 1, 2, 3, 4, 5);
-    writer << stf::protocols::TileLink::makeTransactionWithDelta<stf::protocols::tilelink::ChannelB>(id_manager, 10, 6, 7, 8, 9, 10, std::vector<uint8_t>({0x1, 0x2, 0x3, 0x4}), std::vector<uint8_t>({1, 0, 1, 0}));
-    writer << stf::protocols::TileLink::makeTransactionWithDelta<stf::protocols::tilelink::ChannelC>(id_manager, 20, 11, 12, 13, 14, 15, std::vector<uint8_t>({0x4, 0x3, 0x2, 0x1}));
-    writer << stf::protocols::TileLink::makeTransactionWithDelta<stf::protocols::tilelink::ChannelD>(id_manager, 30, 16, 17, 18, 19, 20, std::vector<uint8_t>({0xab, 0xcd}));
+    writer << stf::protocols::TileLink::makeTransaction<stf::protocols::tilelink::ChannelA>(id_manager, 1, 2, 3, 4, stf::type_utils::none, 5, stf::type_utils::none);
+    writer << stf::protocols::TileLink::makeTransactionWithDelta<stf::protocols::tilelink::ChannelB>(id_manager, 10, 6, 7, 8, 9, std::vector<uint8_t>({0x1, 0x2, 0x3, 0x4}), 10, std::vector<uint8_t>({1, 0, 1, 0}));
+    writer << stf::protocols::TileLink::makeTransactionWithDelta<stf::protocols::tilelink::ChannelC>(id_manager, 20, 11, 12, 13, 14, std::vector<uint8_t>({0x4, 0x3, 0x2, 0x1}), 15);
+    writer << stf::protocols::TileLink::makeTransactionWithDelta<stf::protocols::tilelink::ChannelD>(id_manager, 30, 16, 17, 18, 19, std::vector<uint8_t>({0xab, 0xcd}), 20);
     writer << stf::protocols::TileLink::makeTransactionWithDelta<stf::protocols::tilelink::ChannelE>(id_manager, 40, 21);
 
     {
-        auto transaction_with_metadata = stf::protocols::TileLink::makeTransaction<stf::protocols::tilelink::ChannelA>(id_manager, 1, 2, 3, 4, 5);
+        auto transaction_with_metadata = stf::protocols::TileLink::makeTransaction<stf::protocols::tilelink::ChannelA>(id_manager, 1, 2, 3, 4, stf::type_utils::none, 5, stf::type_utils::none);
 
         transaction_with_metadata.getMetadata().append<uint8_t>(0xcc);
         transaction_with_metadata.getMetadata().append<uint8_t>(0xcc);
@@ -36,7 +36,7 @@ int main(int argc, char** argv) {
 
     // Same as the last transaction, but change the metadata type to uint64_t and append a single uint8_t
     {
-        auto transaction_with_metadata = stf::protocols::TileLink::makeTransaction<stf::protocols::tilelink::ChannelA>(id_manager, 1, 2, 3, 4, 5);
+        auto transaction_with_metadata = stf::protocols::TileLink::makeTransaction<stf::protocols::tilelink::ChannelA>(id_manager, 1, 2, 3, 4, stf::type_utils::none, 5, stf::type_utils::none);
 
         transaction_with_metadata.getMetadata().append<uint64_t>(0xffffeeeeddddcccc);
         transaction_with_metadata.getMetadata().append<uint8_t>(0xaa);
@@ -51,6 +51,9 @@ int main(int argc, char** argv) {
     for(const auto& rec: reader) {
         std::cout << rec;
     }
+}
 
+int main(int argc, char** argv) {
+    tilelink_example();
     return 0;
 }
