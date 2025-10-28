ef4b284e97a8630628fe33e1b94c8780324b6144
Brett Dutro <brett.dutro@gmail.com>
2025-10-28 13:54:34 -0500

    Add a make package target that bundles Mavis JSONs and STF tool binaries into a single ZIP file

--- cmake/mavis_setup.cmake
+++ cmake/mavis_setup.cmake
@@ -1,4 +1,6 @@
-add_subdirectory(${STF_TOOLS_BASE}/mavis)
+set(MAVIS_DIRECTORY ${STF_TOOLS_BASE}/mavis)
+
+add_subdirectory(${MAVIS_DIRECTORY})
 
 if(CMAKE_CXX_COMPILER_ID MATCHES GNU)
     set(EXTRA_MAVIS_COMPILER_FLAGS -Wno-template-id-cdtor)
@@ -7,3 +9,5 @@ else()
 endif()
 
 target_compile_options(mavis PRIVATE -Wno-shorten-64-to-32 -Wno-sign-conversion -Wno-conversion ${EXTRA_MAVIS_COMPILER_FLAGS})
+
+install(DIRECTORY ${MAVIS_DIRECTORY}/json DESTINATION ${STF_INSTALL_DIR}/../share/stf_tools/mavis)
