6c8c8373f0e74c0d15ea8d7db3c7333b775d5e96
Brett Dutro <bdutro@mips.com>
2026-02-13 11:27:45 -0600

    Check if rapidjson target already exists before defining a new one

--- cmake/rapidjson.cmake
+++ cmake/rapidjson.cmake
@@ -1,4 +1,7 @@
 find_package (RapidJSON REQUIRED)
 
-add_library(rapidjson INTERFACE)
-target_include_directories(rapidjson SYSTEM INTERFACE ${RapidJSON_INCLUDE_DIR})
+# Rapidjson didn't have a CMake target defined until recently
+if(NOT TARGET rapidjson)
+    add_library(rapidjson INTERFACE)
+    target_include_directories(rapidjson SYSTEM INTERFACE ${RapidJSON_INCLUDE_DIR})
+endif()
