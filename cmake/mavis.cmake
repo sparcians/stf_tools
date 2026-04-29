45ecb2e38dd1afe071dd0fe46d369f7cb167a5bc
Brett Dutro <bdutro@mips.com>
2026-04-29 11:02:01 -0500

    Use target_compile_options instead of add_compile_options so we don't pollute the flags for submodules

--- cmake/mavis.cmake
+++ cmake/mavis.cmake
@@ -6,12 +6,4 @@ endif()
 
 add_subdirectory(${MAVIS_DIRECTORY} SYSTEM)
 
-if(CMAKE_CXX_COMPILER_ID MATCHES GNU)
-    set(EXTRA_MAVIS_COMPILER_FLAGS -Wno-template-id-cdtor)
-else()
-    set(EXTRA_MAVIS_COMPILER_FLAGS )
-endif()
-
-target_compile_options(mavis PRIVATE -Wno-pedantic -Wno-shorten-64-to-32 -Wno-sign-conversion -Wno-conversion ${EXTRA_MAVIS_COMPILER_FLAGS})
-
 install(DIRECTORY ${MAVIS_DIRECTORY}/json DESTINATION ${STF_INSTALL_DIR}/../share/stf_tools/mavis)
